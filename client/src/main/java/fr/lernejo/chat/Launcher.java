a64caef1738607b466df5ccf6b2b32c6facd280a
thomas-mrx <thomas.moreaux@icloud.com>
2022-01-26 11:31:43 +0100

    Add client

---
+++ client/src/main/java/fr/lernejo/chat/Launcher.java
@@ -0,0 +1,31 @@
+package fr.lernejo.chat;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class Launcher {
+    public static void main(String[] args) {
+
+        String message = "";
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Launcher.class);
+        RabbitTemplate rabbitTemplate = context.getBean(RabbitTemplate.class);
+        rabbitTemplate.setRoutingKey("chat_messages");
+
+        System.out.println("Input a message, we will sent it for you (q for quit)");
+        do {
+            message = scanner.nextLine();
+            quit = message.equals("q");
+            if (!quit && !message.equals("")){
+                rabbitTemplate.convertAndSend(message);
+                System.out.println("Message sent. Input a message, we will sent it for you (q for quit)");
+            }
+        } while (!quit);
+        System.out.println("Bye");
+    }
+}
